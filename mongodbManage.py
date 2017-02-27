b125d3c1b09c2eca3307d5464601667446f95c82
赵兴壮 <834916321@qq.com>
2017-02-27 09:50:24 +0800

    数据隐藏

--- mongodbManage.py
+++ mongodbManage.py
@@ -18,8 +18,8 @@ class MONGODB:
 
     def connect(self):
         user = "admin"
-        pwd = "qiangbi123"
-        server = "115.28.161.44"
+        pwd = ""
+        server = ""
         port = '27017'
         # 表示 用于授权的
         db_name = "admin"
